571186ccbe0344ec4f0560211ff675126108d570
rohitkumar0099 <srohitkumar147@gmail.com>
2022-12-13 10:00:43 +0530

    added line strike on task if marked as completed/checked

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const taskInput = document.querySelector(".task-input input");
+const taskBox = document.querySelector(".task-box");
 
 // Getting localstorage todo-list
 let todos = JSON.parse(localStorage.getItem("todo-list"));
 
-function showTodo() {
-    todos.forEach((element, id) => {
-        console.log(id, element);
-    });
+function addTodo() {
+    let li = "";
+    if (todos) {
+        todos.forEach((todo, id) => {
+            li += `<li class="task">
+                        <label for="${id}">
+                            <input onclick="updateStatus(this)" type="checkbox" id="${id}" />
+                            <p>${todo.name}</p>
+                        </label>
+                        <div class="settings">
+                            <i class="uil uil-ellipsis-h"></i>
+                            <ul class="task-menu">
+                                <li><i class="uil uil-pen">Edit</i></li>
+                                <li><i class="uil uil-trash">Delete</i></li>
+                            </ul>
+                        </div>
+                    </li>`;
+        });
+        taskBox.innerHTML = li;
+    }
+}
+
+function updateStatus(selectedTask) {
+    // to get the task name of the selected task
+    let taskName = selectedTask.parentElement.lastElementChild;
+    console.log(taskName);
+    if (selectedTask.checked) {
+        taskName.classList.add("checked");
+    } else {
+        taskName.classList.remove("checked");
+    }
 }
 
 taskInput.addEventListener("keyup", (e) => {
@@ -20,6 +48,6 @@ taskInput.addEventListener("keyup", (e) => {
         let taskInfo = { name: userTask, status: "pending" };
         todos.push(taskInfo); //adding new task to todos
         localStorage.setItem("todo-list", JSON.stringify(todos));
-        showTodo();
+        addTodo();
     }
 });
